42dd130cfed1e8b6b8b8859c6ce5acb2389788ea
vitoutry <vitoutry@gmail.com>
2023-03-11 22:32:28 +0700

    fix missing component ....and icon path

--- src/main/java/com/example/javafx_librarymanagementsystem/LoginController.java
+++ src/main/java/com/example/javafx_librarymanagementsystem/LoginController.java
@@ -7,7 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
+//import java.awt.event.ActionEvent;
 
 public class LoginController {
 
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.javafx_librarymanagementsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.javafx_librarymanagementsystem to javafx.fxml;
+    exports com.example.javafx_librarymanagementsystem;
+}
