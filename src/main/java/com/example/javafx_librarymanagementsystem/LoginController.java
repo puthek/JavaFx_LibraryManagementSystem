32cc1ce1326b04f67887100bf00a03c17c23a0d2
Hear Leapheng <leapheng.hear@student.cadt.edu.kh>
2023-03-15 17:58:41 +0700

    hi

--- src/main/java/com/example/javafx_librarymanagementsystem/LoginController.java
+++ src/main/java/com/example/javafx_librarymanagementsystem/LoginController.java
@@ -53,8 +53,6 @@ public class LoginController implements Initializable {
     @FXML
     private void handleCancelButtonActionLogin(ActionEvent event)throws IOException{
 
-
-
         Parent fxmlLoader = FXMLLoader.load(getClass().getResource("Login.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(fxmlLoader);
@@ -76,6 +74,18 @@ public class LoginController implements Initializable {
     }
 
 
+    @FXML
+    private void borrowBookController(ActionEvent event)throws IOException{
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Cancel");
+        alert.setHeaderText("Register");
+        Parent fxmlLoader = FXMLLoader.load(getClass().getResource("borrowBook.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader);
+        stage.setScene(scene);
+        stage.show();
+        stage.toBack();
+    }
 
 
 
---
+++ src/main/java/com/example/javafx_librarymanagementsystem/borrowBookController.java
@@ -0,0 +1,33 @@
+package com.example.javafx_librarymanagementsystem;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ResourceBundle;
+//import static jdk.jpackage.internal.WixAppImageFragmentBuilder.ShortcutsFolder.Desktop;
+public class borrowBookController implements Initializable {
+    //FileChooser fileChooser = new FileChooser();
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    void getText(MouseEvent event){
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //  fileChooser.setInitialDirectory(new File())
+    }
+    public void Register(ActionEvent event) throws URISyntaxException, IOException {
+        System.out.println("Click the Register Button ");
+    }
+
+    private Hyperlink hyperlink;
+
+}
